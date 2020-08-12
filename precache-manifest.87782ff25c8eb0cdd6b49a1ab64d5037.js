4aaf3ae0f43f83537321dec3f0d19817bea007a5
arymantar321 <arymantar321@gmail.com>
2020-08-12 20:45:03 +0900

    Updates

---
+++ precache-manifest.87782ff25c8eb0cdd6b49a1ab64d5037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff50f50e1757fd25520602db2aa75bb",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c0b5581cba3656f1c866",
+    "url": "/movie_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "109356d05735fcb9e967",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.670a5eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b5581cba3656f1c866",
+    "url": "/movie_app/static/js/main.0344ed29.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
